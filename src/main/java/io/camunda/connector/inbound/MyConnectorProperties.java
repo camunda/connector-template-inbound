ab8519222051970a96c6dbef7fe7ae1d4e21d98b
markfarkas-camunda <mark.farkas@camunda.com>
2023-08-07 18:09:41 +0200

    chore: fix validation imports

--- src/main/java/io/camunda/connector/inbound/MyConnectorProperties.java
+++ src/main/java/io/camunda/connector/inbound/MyConnectorProperties.java
@@ -1,8 +1,8 @@
 package io.camunda.connector.inbound;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 /**
  * Configuration properties for inbound Connector
