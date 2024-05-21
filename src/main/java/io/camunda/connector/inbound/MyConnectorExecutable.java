cecaba087fe18a4c05cd73ee178670d8b7029643
Pavel Kotelevsky <pavel.kotelevskii@camunda.com>
2024-05-21 22:25:11 +0200

    chore(deps): bump connector SDK to 8.6

--- src/main/java/io/camunda/connector/inbound/MyConnectorExecutable.java
+++ src/main/java/io/camunda/connector/inbound/MyConnectorExecutable.java
@@ -1,10 +1,17 @@
 package io.camunda.connector.inbound;
 
 import io.camunda.connector.api.annotation.InboundConnector;
+import io.camunda.connector.api.inbound.CorrelationFailureHandlingStrategy.ForwardErrorToUpstream;
+import io.camunda.connector.api.inbound.CorrelationFailureHandlingStrategy.Ignore;
+import io.camunda.connector.api.inbound.CorrelationResult;
+import io.camunda.connector.api.inbound.CorrelationResult.Failure;
+import io.camunda.connector.api.inbound.CorrelationResult.Success;
 import io.camunda.connector.api.inbound.InboundConnectorContext;
 import io.camunda.connector.api.inbound.InboundConnectorExecutable;
 import io.camunda.connector.inbound.subscription.MockSubscription;
 import io.camunda.connector.inbound.subscription.MockSubscriptionEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @InboundConnector(name = "My Inbound Connector", type = "io.camunda:my-inbound-connector:1")
 public class MyConnectorExecutable implements InboundConnectorExecutable<InboundConnectorContext> {
@@ -12,6 +19,8 @@ public class MyConnectorExecutable implements InboundConnectorExecutable<Inbound
   private MockSubscription subscription;
 
   private InboundConnectorContext context;
+
+  private final static Logger LOG = LoggerFactory.getLogger(MyConnectorExecutable.class);
 
   @Override
   public void activate(InboundConnectorContext connectorContext) {
@@ -21,7 +30,26 @@ public class MyConnectorExecutable implements InboundConnectorExecutable<Inbound
   }
 
   private void onEvent(MockSubscriptionEvent rawEvent) {
-    context.correlate(new MyConnectorEvent(rawEvent));
+    var result = context.correlateWithResult(new MyConnectorEvent(rawEvent));
+    handleResult(result);
+  }
+
+  private void handleResult(CorrelationResult result) {
+    switch (result) {
+      case Success ignored -> LOG.debug("Message correlated successfully");
+      case Failure failure -> {
+        switch (failure.handlingStrategy()) {
+          case ForwardErrorToUpstream ignored -> {
+            LOG.error("Correlation failed, reason: {}", failure.message());
+            // forward error to upstream
+          }
+          case Ignore ignored -> {
+            LOG.debug("Correlation failed but no action required, reason: {}", failure.message());
+            // ignore
+          }
+        }
+      }
+    }
   }
 
   @Override
