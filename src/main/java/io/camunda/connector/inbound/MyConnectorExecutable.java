8789200a0ed967dd4b79e492b716bfbb28065557
Mathias Vandaele <mathias.vandaele@camunda.com>
2024-12-27 16:37:32 +0100

    feature(inbound-template): refactor the inbound template

--- src/main/java/io/camunda/connector/inbound/MyConnectorExecutable.java
+++ src/main/java/io/camunda/connector/inbound/MyConnectorExecutable.java
@@ -3,10 +3,22 @@ package io.camunda.connector.inbound;
 import io.camunda.connector.api.annotation.InboundConnector;
 import io.camunda.connector.api.inbound.InboundConnectorContext;
 import io.camunda.connector.api.inbound.InboundConnectorExecutable;
+import io.camunda.connector.generator.java.annotation.ElementTemplate;
 import io.camunda.connector.inbound.subscription.MockSubscription;
 import io.camunda.connector.inbound.subscription.MockSubscriptionEvent;
 
 @InboundConnector(name = "My Inbound Connector", type = "io.camunda:my-inbound-connector:1")
+@ElementTemplate(
+        id = "io.camunda.connector.Template.v1",
+        name = "Template connector",
+        version = 1,
+        description = "Describe this connector",
+        icon = "icon.svg",
+        documentationRef = "https://docs.camunda.io/docs/components/connectors/out-of-the-box-connectors/available-connectors-overview/",
+        propertyGroups = {
+                @ElementTemplate.PropertyGroup(id = "properties", label = "Properties"),
+        },
+        inputDataClass = MyConnectorProperties.class)
 public class MyConnectorExecutable implements InboundConnectorExecutable<InboundConnectorContext> {
 
   private MockSubscription subscription;
@@ -21,7 +33,7 @@ public class MyConnectorExecutable implements InboundConnectorExecutable<Inbound
   }
 
   private void onEvent(MockSubscriptionEvent rawEvent) {
-    context.correlate(new MyConnectorEvent(rawEvent));
+    context.correlateWithResult(new MyConnectorEvent(rawEvent));
   }
 
   @Override
